d802007f17839ad92dd39348d8e1a7d4950399f9
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2024-08-17 15:55:02 +0530

    Time: 228 ms (66.51%) | Memory: 107.9 MB (29.25%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    int func(int i,int j,vector<pair<int,int>>&vp){
-        if(vp[i].second!=vp[j].second)
-        return abs(vp[i].first-vp[j].first);
-        
-        
-        return max(func(i+1,j,vp),func(i,j-1,vp));
-    }
     int maxDistance(vector<vector<int>>& arrays) {
-        int n=arrays.size();
-        vector<pair<int,int>>vp;//element,index
-        for(int i=0;i<n;i++){
-            int N=arrays[i].size();
-            vp.push_back({arrays[i][0],i});
-            vp.push_back({arrays[i][N-1],i});
-        }
-        sort(vp.begin(),vp.end());
-        cout<<"vp"<<endl;
-        for(auto &x:vp){
-            cout<<x.first<<" "<<x.second<<endl;
-        }
-        int nu=vp.size();
-        return func(0,nu-1,vp);
+       int mx=arrays[0].back();
+       int mn=arrays[0].front();
+       //abhi tak ka minimum and maximum
+       int result=0;
+       for(int i=1;i<arrays.size();i++){
+        int cmn=arrays[i].front();
+        int cmx=arrays[i].back();
+        result=max(result,abs(cmx-mn));
+        result=max(result,abs(mx-cmn));
+        mx=max(mx,cmx);
+        mn=min(mn,cmn);
+       }
+       cout<<result<<endl;
+       return result; 
     }
 };
