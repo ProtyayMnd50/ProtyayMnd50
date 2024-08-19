ef01ba40a40a56163f0a6451b36fa0bf227146da
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2024-08-20 01:37:05 +0530

    Time: 15 ms (36.83%) | Memory: 7.6 MB (56.27%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -2,18 +2,19 @@
 class Solution {
 public:
 //cur--> in clipboard
-    ll func(ll n,ll val,ll cur){
-        if(val>n)
-        return 1e9;
-        if(n==val)
-        return 0;
-        ll st1=2+func(n,val+val,val);
-        ll st2=1+func(n,val+cur,cur);
-        return min(st1,st2);
+//val->current value
+    int func(int val,int clip,int n){
+        if(val>n)return 1e9;
+        if(val==n)return 0;
+
+        int copypaste=2+func(val+val,val,n);
+        int paste=1+func(val+clip,clip,n);
+        return min(copypaste,paste);
     }
     int minSteps(int n) {
         if(n==1)
         return 0;
-        return 1+func(n,1,1);
+
+        return 1+func(1,1,n);
     }
 };
