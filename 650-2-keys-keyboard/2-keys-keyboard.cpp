cdbdd1a3adb6e3e38634d1a0dea78525a73dd0af
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2024-08-21 01:32:57 +0530

    Time: 5 ms (44.60%) | Memory: 8.2 MB (24.97%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,20 +1,34 @@
-#define ll long long
 class Solution {
 public:
-//cur--> in clipboard
-//val->current value
-    int func(int val,int clip,int n){
-        if(val>n)return 1e9;
-        if(val==n)return 0;
-
-        int copypaste=2+func(val+val,val,n);
-        int paste=1+func(val+clip,clip,n);
-        return min(copypaste,paste);
-    }
     int minSteps(int n) {
-        if(n==1)
-        return 0;
+        if (n == 1)
+            return 0;
 
-        return 1+func(1,1,n);
+        if (n == 2)
+            return 2;
+
+        vector<int> t(n + 1, 0);
+
+        t[0] = 0;
+        t[1] = 0;
+        t[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+
+            int factor = i / 2; // start from largest factor
+            while (factor >= 1) {
+                if (i % factor == 0) {
+
+                    int steps_to_reach_factor = t[factor];
+                    int copy_that_first = 1;
+                    int paste_frequency = (i / factor) - 1;
+                    t[i] = t[factor] + copy_that_first + paste_frequency;
+                    break;
+                }
+                factor--;
+            }
+        }
+
+        return t[n];
     }
 };
