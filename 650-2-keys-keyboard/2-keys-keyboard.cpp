bc26694fa7088c0c476be4314bd891adf7ee8ecf
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2024-08-21 01:35:16 +0530

    Time: 3 ms (64.98%) | Memory: 8 MB (37.53%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,34 +1,42 @@
+#define ll long long
 class Solution {
 public:
+//cur--> in clipboard
+//val->current value
+    int func(int val,int clip,int n,vector<vector<int>>&dp){
+        if(val>n)return 1e9;
+        if(val==n)return 0;
+        if(dp[val][clip]!=-1)return dp[val][clip];
+        int copypaste=2+func(val+val,val,n,dp);
+        int paste=1+func(val+clip,clip,n,dp);
+        return dp[val][clip]=min(copypaste,paste);
+    }
     int minSteps(int n) {
-        if (n == 1)
-            return 0;
+        // if(n==1)
+        // return 0;
+        // vector<vector<int>>dp(1010,vector<int>(1010,-1));
+        // return 1+func(1,1,n,dp);
 
-        if (n == 2)
-            return 2;
-
-        vector<int> t(n + 1, 0);
-
-        t[0] = 0;
-        t[1] = 0;
-        t[2] = 2;
-
-        for (int i = 3; i <= n; i++) {
-
-            int factor = i / 2; // start from largest factor
-            while (factor >= 1) {
-                if (i % factor == 0) {
-
-                    int steps_to_reach_factor = t[factor];
-                    int copy_that_first = 1;
-                    int paste_frequency = (i / factor) - 1;
-                    t[i] = t[factor] + copy_that_first + paste_frequency;
+        //Top-down approach
+        vector<int>dp(1001,0);
+        dp[0]=0;
+        dp[1]=0;
+        // dp[2]=2;
+        if(n>=2){
+        for(int i=2;i<=n;i++){
+            int fact=i/2;//trying to start from the largest divisor
+            while(fact>=1){
+                if(i%fact==0){//largest divisor
+                    int fact_steps=dp[fact];
+                    int copy=1;//one copy operation
+                    int pasteOps=(i/fact-1);//itne paste operations
+                    dp[i]=fact_steps+pasteOps+copy;
                     break;
                 }
-                factor--;
+                fact--;
             }
         }
-
-        return t[n];
+        }
+        return dp[n];
     }
 };
